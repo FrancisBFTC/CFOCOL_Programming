a577fc9019813756a76bcc22d4af4044b7ff5227
FrancisBFTC <bftcorporations@gmail.com>
2020-08-18 13:47:45 -0300

    Codigo-fonte do interpretador MAIN

--- Source/cfocol.cpp
+++ Source/cfocol.cpp
@@ -7,7 +7,7 @@ void PreInterpreter(void);
 
 int main(int argc, char *argv[ ]){
 
-	
+	if(argc > 1){
      if(cont(argv[1], ".cf") || cont(argv[1], ".cfl") || cont(argv[1], ".cop")){
 		
 		readFile = fopen(argv[1], "r");
@@ -39,15 +39,20 @@ int main(int argc, char *argv[ ]){
 			}
 			
 		}else{
-			printf("\nErro na abertura do arquivo '%s'\n", argv[1]);
+			printf("\nErro na abertura do arquivo '%s'\n\n", argv[1]);
   			return 0;
 		}
   		
   		fclose(readFile);
   		
 	 }else{
-	 	printf("\nO arquivo nao e interpretavel pelo CFOCOL\n");
+	 	printf("\nO arquivo nao e interpretavel pelo CFOCOL\n\n");
 	 }
+	}else{
+		printf("\nO Interpretador espera argumentos...\n");
+		printf("Nenhum arquivo pra interpretar!\n\n");
+		system("pause");
+	}
 	 
  return 0;
 }
